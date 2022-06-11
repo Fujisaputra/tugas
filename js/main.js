85c0421c8b930baa33e486ba01303260aa5267f9
Terima Kasih Telah Kembali <fujisaputra@pop-os.localdomain>
2022-06-11 21:36:55 +0700

    tugas akhir

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+
+/*testimonials*/
+function testimonialSlider(){
+    const carouselOne = document.getElementById('carouselOne');
+    if(carouselOne){
+        carouselOne.addEventListener('slid.bs.carousel', function () {
+            const activeItem = this.querySelector(".active");
+            document.querySelector(".js-testimonial-img").src =
+            activeItem.getAttribute("data-js-testimonial-img");
+       })
+    }
+}
+testimonialSlider();
